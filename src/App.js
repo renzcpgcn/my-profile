bd95c703610b2093086b4fe003d9c9b60bfb3df9
Renz Capagcuan <capagcuanrenz13@gmail.com>
2024-10-21 06:34:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link,  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.css';
-import Home from './Component/Home.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Home from './Component/Home';
 import About from './Component/About';
 import Skills from './Component/Skills';
 import Contact from './Component/Contact';
@@ -13,40 +13,45 @@ const App = () => {
 
   return (
     <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <Link className="navbar-brand" to="/">Renz</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/skills">Skills</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <div className="container mt-4">
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/skills" element={<Skills />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/submission-success" element={<SubmissionSuccess />} />
-            </Routes>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Renz</Link>
+          
+          
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/skills">Skills</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+
+      <div className="container mt-4">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/submission-success" element={<SubmissionSuccess />} />
+        </Routes>
+      </div>
     </Router>
   );
-
 };
 
 export default App;
